600c36ad95d4af5316dc865741c1bb84b7c6e50a
james <jamescarraro@outlook.com>
2026-07-23 22:07:07 +1000

    Flake setup

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Pricing Library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        pythonEnv = pkgs.python3.withPackages (
+          ps: with ps; [
+            numpy
+            pytest
+            scipy
+          ]
+        );
+      in
+      {
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pythonEnv
+          ];
+        };
+      }
+
+    );
+}
